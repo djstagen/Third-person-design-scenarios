182adecfb93c6f95ec1faaea497f4798edbe9fae
EfisEvisule <58088743+djstagen@users.noreply.github.com>
2020-09-24 09:10:47 -0500

    AddedPhysicsMaterials and max speed

--- Assets/Scripts/RigidBodyCharacterController.cs
+++ Assets/Scripts/RigidBodyCharacterController.cs
@@ -10,21 +10,38 @@ public class RigidBodyCharacterController : MonoBehaviour
     [SerializeField]
     private float maxSpeed = 2;
 
+    [SerializeField]
+    private PhysicMaterial stoppingPhysicsMaterial, movingPhysicsMaterial;
+
     private new Rigidbody rigidbody;
     private Vector2 input;
+    private new Collider collider;
 
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
     }
 
     private void FixedUpdate()
     {
         var inputDirection = new Vector3(input.x, 0, input.y);
 
+        collider.material = inputDirection.magnitude > 0 ? movingPhysicsMaterial : stoppingPhysicsMaterial;
+        
+        //the one line above is the same as commented out section below.
+       // if (inputDirection.magnitude > 0)
+       // {
+       //     collider.material = movingPhysicsMaterial;
+       // }
+       // else
+       // {
+       //     collider.material = stoppingPhysicsMaterial;
+       // }
+
         if (rigidbody.velocity.magnitude < maxSpeed)
         {
-        rigidbody.AddForce(inputDirection * accelerationForce);
+        rigidbody.AddForce(inputDirection * accelerationForce, ForceMode.Acceleration);
         }
     }
 
